cb0015f50036a6dd9e05f4af8039fda05e243dd2
HimaMynampati <35256374+HimaMynampati@users.noreply.github.com>
2024-03-27 21:31:06 +0530

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-                System.out.println("test application ####");
+                System.out.println("test appplication ####");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
